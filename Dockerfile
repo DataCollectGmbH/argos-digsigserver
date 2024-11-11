237a87a0d3d8def9c7e6094692b8c18fc51593a4
Islam-Hussein-11 <hussein@datacollect.com>
2024-11-11 13:20:56 +0100

    added entry

--- Dockerfile
+++ Dockerfile
@@ -56,7 +56,7 @@ RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2
 
 
 COPY entrypoint.sh /entrypoint.sh
-RUN chnod +x /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
 
---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Log into AWS
+echo "Logging into AWS..."
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
+# Run the main command
+exec "$@"
